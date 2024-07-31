bd17af2def84eb3e29ac94ca8fa37e634a95f465
cs-ankit-gupta <gankit@fortinet.com>
2024-08-01 02:59:44 +0530

    Added logic to pass the tool choice while calling function

--- openai/assistant_manager.py
+++ openai/assistant_manager.py
@@ -17,12 +17,15 @@ class AssistantManager:
         self.config = config
         self.params = params
         self.message_detail = {}
+        self.tool_choice = "auto"
+        if 'tool_choice' in self.params:
+            self.tool_choice = self.params['tool_choice']
 
     def get_llm_response(self):
         payload = {'thread_id': self.params['thread_id'], 'role': self.params['role'],
                    'content': self.params['content']}
         self.message_detail = create_thread_message(config=self.config, params=payload)
-        assistant_response = self.run_assistant(instructions=self.params['instructions'])
+        assistant_response = self.run_assistant()
         return assistant_response
 
     def run_assistant(self, instructions=""):
@@ -34,7 +37,8 @@ class AssistantManager:
                 thread_id=self.params['thread_id'],
                 assistant_id=self.params['assistant_id'],
                 instructions=instructions,
-                event_handler=event_handler
+                event_handler=event_handler,
+                tool_choice=self.tool_choice
         ) as stream:
             stream.until_done()
         return {"llm_response": event_handler.get_thread_messages(), "token_usage": event_handler.token_usage}
